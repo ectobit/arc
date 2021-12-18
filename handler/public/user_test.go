bd5dda6ae37f58f189693f51b074d0fbcf356ff8
Boban Acimovic <boban.acimovic@gmail.com>
2021-12-18 20:01:26 +0100

    add test password from json

--- handler/public/user_test.go
+++ handler/public/user_test.go
@@ -168,6 +168,53 @@ func TestEmailFromJSON(t *testing.T) {
 	}
 }
 
+func TestPasswordFromJSON(t *testing.T) {
+	t.Parallel()
+
+	log := lax.NewZapAdapter(zaptest.NewLogger(t))
+
+	tests := map[string]struct {
+		in      string
+		want    *public.Password
+		wantErr string
+	}{
+		"invalid json body": {``, nil, "invalid json body"},
+		"empty body":        {`{}`, nil, "empty password"},
+		"empty password":    {`{"password":""}`, nil, "empty password"},
+		"ok": {
+			`{"password":"h+z67{GxLSL~]Cl(I88AqV7w"}`,
+			&public.Password{Password: "h+z67{GxLSL~]Cl(I88AqV7w"}, "",
+		},
+	}
+
+	for n, test := range tests { //nolint:paralleltest
+		test := test
+
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+
+			buf := bytes.NewBufferString(test.in)
+
+			got, gotErr := public.PasswordFromJSON(buf, log)
+			if test.wantErr != "" {
+				if gotErr == nil {
+					t.Fatalf("PasswordFromJSON(%q) = error nil; want error %q", test.in, test.wantErr)
+				}
+
+				if gotErr.Error() != test.wantErr {
+					t.Fatalf("PasswordFromJSON(%q) = error %q; want error %q", test.in, gotErr, test.wantErr)
+				}
+
+				return
+			}
+
+			if got.Password != test.want.Password {
+				t.Errorf("PasswordFromJSON(%q) = %v; want %v", test.in, got, test.want)
+			}
+		})
+	}
+}
+
 func TestFromDomainUser(t *testing.T) {
 	t.Parallel()
 
