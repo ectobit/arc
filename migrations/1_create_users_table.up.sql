45236bda390c5a36df6800fcf72f969f80e91225
Boban Acimovic <boban.acimovic@gmail.com>
2022-05-07 23:43:35 +0200

    add some commented fields which may be used in the future

--- migrations/1_create_users_table.up.sql
+++ migrations/1_create_users_table.up.sql
@@ -10,6 +10,13 @@ CREATE TABLE users (
   activated timestamp with time zone
   created timestamp with time zone DEFAULT current_timestamp NOT NULL,
   updated timestamp with time zone
+  -- confirmation_token varchar(255) NULL,
+	-- confirmation_sent_at timestamptz NULL,
+	-- recovery_sent_at timestamptz NULL,
+	-- email_change_token varchar(255) NULL,
+	-- email_change varchar(255) NULL,
+	-- email_change_sent_at timestamptz NULL,
+	-- last_sign_in_at timestamptz NULL,
 );
 
 COMMENT ON COLUMN users.activation_token IS 'used just for initial account activation';
